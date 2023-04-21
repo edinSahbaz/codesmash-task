4cf5307819b1c22b7c13000701fc7aa749da057b
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-04-21 19:50:10 +0200

    New Tests

--- Solution/CodeSmashTask.Test/SolutionTest.cs
+++ Solution/CodeSmashTask.Test/SolutionTest.cs
@@ -1,9 +1,11 @@
-﻿namespace CodeSmashTask.Test;
-using CodeSmashTask;
+﻿using CodeSmashTask;
+using Xunit;
+
+namespace CodeSmashTask.Test;
 
 public class SolutionTest
 {
-    #region Given Examples Tests
+    #region Given Examples Tests - FROM THE TASK
     [Fact]
     public void IsBuyerWinner_Example1()
     {
@@ -50,6 +52,17 @@ public class SolutionTest
     #endregion
 
     [Fact]
+    public void IsBuyerWinner_Example1Modified()
+    {
+        var codeList = new List<string> { "apple, apple", "banana, anything, banana" };
+        var shoppingCart = new List<string> { "orange", "apple", "apple", "orange", "orange", "banana", "orange", "banana" };
+
+        int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
     public void IsBuyerWinner_Example4Modified()
     {
         var codeList = new List<string> { "apple, apple", "apple, apple, banana" };
@@ -76,6 +89,50 @@ public class SolutionTest
     {
         var codeList = new List<string> { "apple, apple", "apple, apple, banana" };
         var shoppingCart = new List<string> ();
+
+        int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
+
+        Assert.Equal(0, actual);
+    }
+
+    [Fact]
+    public void IsBuyerWinner_WithBothListsEmpty()
+    {
+        var codeList = new List<string>();
+        var shoppingCart = new List<string>();
+
+        int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void IsBuyerWinner_WithOneCodeList()
+    {
+        var codeList = new List<string> { "apple" , "orange" };
+        var shoppingCart = new List<string> { "apple", "orange", "banana", "apple" };
+
+        int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void IsBuyerWinner_WithThreeCodeListSuccess()
+    {
+        var codeList = new List<string> { "apple, orange", "banana, banana, orange", "orange, banana" };
+        var shoppingCart = new List<string> { "apple", "orange", "banana", "apple", "banana", "banana", "orange", "orange", "banana" };
+
+        int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void IsBuyerWinner_WithThreeCodeListFail()
+    {
+        var codeList = new List<string> { "apple, orange", "banana, banana, orange", "orange, banana" };
+        var shoppingCart = new List<string> { "apple", "orange", "banana", "apple", "banana", "banana", "orange", "banana" };
 
         int actual = CodeSmashTask.ProductPromotionCalculator.IsBuyerWinner(codeList, shoppingCart);
 
